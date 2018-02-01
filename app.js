fca21acddcb070c550076613ed273594600b4a12
Nikitha <a614343@fmr.com>
2018-02-01 18:02:58 +0530

    VidJot Project Start & Express Fundamentals

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req,res) => {
-    res.send('hello');
+    const title = 'welcome to the world!'
+    res.render('index', {
+        title: title
+    });
 });
 app.get('/about', (req, res) => {
-  res.send('about');
+  res.render('about');
 });
 const port = 5000;
 app.listen(port, () => {
